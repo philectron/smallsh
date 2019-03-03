e1189d28f437fa2d136434906213dff0aee02cb4
Phi Luu <philectron@users.noreply.github.com>
2019-03-03 02:03:46 -0800

    Implement a dynamic array of PIDs

--- program3_smallsh/utility.c
+++ program3_smallsh/utility.c
@@ -34,43 +34,6 @@ void InitDynStrArr(DynStrArr* arr, int capacity) {
     arr->capacity = capacity;
 }
 
-/*
-// Doubles the capacity of a dynamic array of strings.
-//
-// Argument:
-//   arr  the structure of dynamic array of strings, whose capacity to double
-//
-// The array of strings inside  arr  will have a double capacity.
-void DoubleDynStrArrCapacity(DynStrArr* arr) {
-    assert(arr && arr->strings);
-
-    // init a new array of strings with double the capacity
-    char** new_strings = malloc(arr->capacity * 2 * sizeof(*new_strings));
-    assert(new_strings);
-
-    // add all strings to the new array and deallocate the old
-    for (int i = 0; i < arr->size; i++) {
-        // in case of stumbling upon a NULL element
-        if (!arr->strings[i]) {
-            // fail-safe, as NULL will always be put at the end of the array
-            new_strings[i] = NULL;
-        } else {
-            new_strings[i] = malloc((strlen(arr->strings[i]) + 1) *
-                                     sizeof(*(new_strings[i])));
-            strcpy(new_strings[i], arr->strings[i]);
-            free(arr->strings[i]);
-        }
-    }
-
-    // destroy the old array of strings
-    free(arr->strings);
-    arr->strings = new_strings;
-
-    // update capacity
-    arr->capacity *= 2;
-}
-*/
-
 // Pushes a string to the back of the dynamic array (double the capacity if
 // needed).
 //
@@ -84,16 +47,15 @@ void PushBackDynStrArr(DynStrArr* arr, char* new_string) {
     assert(arr && arr->strings && new_string);
 
     // before adding, must double the capacity if array is full
-    /* if (arr->size == arr->capacity) DoubleDynStrArrCapacity(arr); */
     if (arr->size == arr->capacity) {
-        arr->strings = realloc(arr->strings, arr->capacity * 2 *
-                               sizeof(*(arr->strings)));
+        arr->strings = realloc(arr->strings, arr->capacity * 2
+                               * sizeof(*(arr->strings)));
         arr->capacity *= 2;
     }
 
     // allocate memory and copy the new string to the back of the array
-    arr->strings[arr->size] = malloc((strlen(new_string) + 1) *
-                                     sizeof(*(arr->strings[arr->size])));
+    arr->strings[arr->size] = malloc((strlen(new_string) + 1)
+                                     * sizeof(*(arr->strings[arr->size])));
     assert(arr->strings[arr->size]);
     strcpy(arr->strings[arr->size], new_string);
     arr->size++;
@@ -108,10 +70,9 @@ void PushBackNullDynStrArr(DynStrArr* arr) {
     assert(arr);
 
     // before adding, must double the capacity if the array is full
-    /* if (arr->size == arr->capacity) DoubleDynStrArrCapacity(arr); */
     if (arr->size == arr->capacity) {
-        arr->strings = realloc(arr->strings, arr->capacity * 2 *
-                               sizeof(*(arr->strings)));
+        arr->strings = realloc(arr->strings, arr->capacity * 2
+                               * sizeof(*(arr->strings)));
         arr->capacity *= 2;
     }
 
@@ -122,7 +83,7 @@ void PushBackNullDynStrArr(DynStrArr* arr) {
 // Deallocates the array of strings in the structure and resets the parameters.
 //
 // Argument:
-//   arr  the structure of dynamica array of strings to be deallocated
+//   arr  the structure of dynamic array of strings to be deallocated
 //
 // arr->strings  will be freed, and  arr->size  and arr->capacity  will be reset
 // to zero.
@@ -136,6 +97,64 @@ void DeleteDynStrArr(DynStrArr* arr) {
 
     // clean up the array and reset parameters
     free(arr->strings);
+    arr->size = 0;
+    arr->capacity = 0;
+}
+
+// Dynamically allocates an array of PIDs structure with the given capacity.
+//
+// Argument:
+//   capacity  the initial capacity of the array
+//
+// Returns:
+//   A newly allocated structure of dynamic array of PIDs
+void InitDynPidArr(DynPidArr* arr, int capacity) {
+    assert(arr && capacity > 0);
+
+    // allocate dynamic memory for the array
+    arr->pids = malloc(capacity * sizeof(*(arr->pids)));
+    assert(arr->pids);  // make sure allocation was succesful
+
+    // init the array's size and capacity
+    arr->size = 0;
+    arr->capacity = capacity;
+}
+
+// Pushes a PID to the back of the dynamic array (double the capacity if
+// needed).
+//
+// Arguments:
+//   arr      the structure of dynamic array of PIDs
+//   new_pid  the PID to be pushed to the back of  arr->pids
+//
+// The array of PIDs inside  arr  will have an additional PID at its back.
+// The capacity of the array will be doubled if necessary.
+void PushBackDynPidArr(DynPidArr* arr, pid_t new_pid) {
+    assert(arr && arr->pids && new_pid > 0);
+
+    // before adding, must double the capacity if array is full
+    if (arr->size == arr->capacity) {
+        arr->pids = realloc(arr->pids, arr->capacity * 2
+                            * sizeof(*(arr->pids)));
+        arr->capacity *= 2;
+    }
+
+    arr->pids[arr->size++] = new_pid;
+}
+
+// Deallocates the array of PIDs in the structure and resets the parameters.
+//
+// Argument:
+//   arr  the structure of dynamic array of PIDs to be deallocated
+//
+// arr->pids  will be freed, and  arr->size  and arr->capacity  will be reset
+// to zero.
+void DeleteDynPidArr(DynPidArr* arr) {
+    // do nothing if the structure or the array is NULL
+    if (!arr) return;
+
+    // clean up the array and reset parameters
+    free(arr->pids);
     arr->size = 0;
     arr->capacity = 0;
 }
--- program3_smallsh/utility.h
+++ program3_smallsh/utility.h
@@ -13,20 +13,29 @@
 #ifndef UTILITY_H_
 #define UTILITY_H_
 
+#include <sys/types.h>
+
+// a dynamic array that can hold a command line and its arguments
 typedef struct DynStrArr {
     char** strings;
     int size;
     int capacity;
 } DynStrArr;
 
+// a dynamic array that can hold children PIDs of a program
+typedef struct DynPidArr {
+    pid_t* pids;
+    int size;
+    int capacity;
+} DynPidArr;
+
 void InitDynStrArr(DynStrArr* arr, int capacity);
-
-/* void DoubleDynStrArrCapacity(DynStrArr* arr); */
-
 void PushBackDynStrArr(DynStrArr* arr, char* new_string);
-
 void PushBackNullDynStrArr(DynStrArr* arr);
-
 void DeleteDynStrArr(DynStrArr* arr);
+
+void InitDynPidArr(DynPidArr* arr, int capacity);
+void PushBackDynPidArr(DynPidArr* arr, pid_t new_pid);
+void DeleteDynPidArr(DynPidArr* arr);
 
 #endif  // #ifndef UTILITY_H_
